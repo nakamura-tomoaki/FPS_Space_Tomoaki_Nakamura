8f3062c1cef8198cd43c7ddff44de009e0b53a51
Tomoaki_Nakamura <nakamura213tomoaki@gmail.com>
2017-04-04 22:07:09 +0900

    Upgraded fire effect

--- Assets/Scripts/GunManager.cs
+++ Assets/Scripts/GunManager.cs
@@ -8,9 +8,9 @@ public class GunManager : MonoBehaviour {
 	[SerializeField] int bullet_box = 150;
 	[SerializeField] float cool_time = 0.5f;
 	float timer = 0.0f;
-
+	GameObject fire_effect;
 	void Start () {
-		
+		fire_effect = Resources.Load<GameObject> ("Fire_effect");
 	}
 	
 	
@@ -27,5 +27,7 @@ public class GunManager : MonoBehaviour {
 		print("Fire!!");
 		bullet--;
 		timer = 0f;
+		GameObject fire_effect_instance = Instantiate(fire_effect,transform.position,transform.rotation);
+		Destroy (fire_effect_instance, 1.0f);
 	}
 }
