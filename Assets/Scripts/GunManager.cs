c399836239f92b3f177242521b70742cf83bec24
Tomoaki_Nakamura <nakamura213tomoaki@gmail.com>
2017-04-04 20:58:09 +0900

    Made effect used when player fires

---
+++ Assets/Scripts/GunManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunManager : MonoBehaviour {
+
+	[SerializeField] int bullet = 30;
+	[SerializeField] int bullet_box = 150;
+	[SerializeField] float cool_time = 0.5f;
+	float timer = 0.0f;
+
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+		if (Input.GetMouseButtonDown (0) && bullet > 0 && timer > cool_time) {
+			Fire ();
+		}
+
+		timer += Time.deltaTime;
+
+	}
+
+	void Fire(){
+		print("Fire!!");
+		bullet--;
+		timer = 0f;
+	}
+}
