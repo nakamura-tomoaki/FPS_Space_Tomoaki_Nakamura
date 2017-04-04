d9132d48c48cbfda59b71f32a1058b4bb0502717
Tomoaki_Nakamura <nakamura213tomoaki@gmail.com>
2017-04-04 22:39:21 +0900

    Wrote script for instantiate effect and play audio and render ray

--- Assets/Scripts/GunManager.cs
+++ Assets/Scripts/GunManager.cs
@@ -6,11 +6,16 @@ public class GunManager : MonoBehaviour {
 
 	[SerializeField] int bullet = 30;
 	[SerializeField] int bullet_box = 150;
-	[SerializeField] float cool_time = 0.5f;
+	[SerializeField] float cool_time = 0.2f;
 	float timer = 0.0f;
 	GameObject fire_effect;
+	AudioSource audioSource;
+	LineRenderer line;
+
 	void Start () {
 		fire_effect = Resources.Load<GameObject> ("Fire_effect");
+		audioSource = GetComponent<AudioSource> ();
+		line = GetComponent<LineRenderer> ();
 	}
 	
 	
@@ -29,5 +34,20 @@ public class GunManager : MonoBehaviour {
 		timer = 0f;
 		GameObject fire_effect_instance = Instantiate(fire_effect,transform.position,transform.rotation);
 		Destroy (fire_effect_instance, 1.0f);
+		audioSource.Play ();
+		line.enabled = true;
+		line.SetPosition (0, transform.position);
+		Ray ray = new Ray (transform.position, transform.forward);
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit)) {
+			line.SetPosition (1, hit.point);
+		} else {
+			line.SetPosition (1, transform.position + transform.forward * 100f);
+		}
+		Invoke ("disableLine", 0.05f);			
+	}
+
+	void disableLine(){
+		line.enabled = false;
 	}
 }
