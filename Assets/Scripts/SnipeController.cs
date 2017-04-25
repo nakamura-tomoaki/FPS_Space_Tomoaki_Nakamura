02e10b199dd2179c5fb2b882be7e992c1789f912
Tomoaki_Nakamura <nakamura213tomoaki@gmail.com>
2017-04-25 21:54:00 +0900

    Make snipe sprite animation and Write SnipeController.cs

---
+++ Assets/Scripts/SnipeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnipeController : MonoBehaviour {
+
+	[SerializeField] Animator camera_anim;
+	[SerializeField] Animator snipe_sprite_anim;
+	string IsSniping = "IsSniping";
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+		camera_anim.SetBool (IsSniping, Input.GetMouseButton(1));
+		snipe_sprite_anim.SetBool (IsSniping, Input.GetMouseButton(1));
+	}
+}
