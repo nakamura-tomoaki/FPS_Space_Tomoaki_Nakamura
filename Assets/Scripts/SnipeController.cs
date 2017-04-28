eea77b79812c93d5baafde243be08f34549e9ee8
Tomoaki_Nakamura <nakamura213tomoaki@gmail.com>
2017-04-28 13:41:00 +0900

    Delete some extla sentences.

--- Assets/Scripts/SnipeController.cs
+++ Assets/Scripts/SnipeController.cs
@@ -8,19 +8,13 @@ public class SnipeController : MonoBehaviour {
 	[SerializeField] Image snipe ;
 	[SerializeField] Color color_snipe;
 	[SerializeField] Color color_default;
-	void Start () {
-		
-	}
-	
-	
+
 	void Update () {
 		if (Input.GetMouseButtonDown (1)) {
-			print (1);
 			Camera.main.fieldOfView = 11.5f;
 			snipe.color = color_snipe;
 		} 
 		if (Input.GetMouseButtonUp (1)) {
-			print (2);
 			Camera.main.fieldOfView = 60f;
 			snipe.color = color_default;
 		}
